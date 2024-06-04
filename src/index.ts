2f0e63e6a146a942da5940ca6e56c11d0495e2cf
Arunkumar Murugesan <arunkumarmurugesan@Arunkumars-MacBook-Air.local>
2024-06-04 20:22:00 +0530

    server code

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,19 @@
 import connectDB from "./connectMongoDB";
 import { mongo_uri } from "./config";
+import express,  {Express} from "express";
+import todoRoutes from "./routes";
 
 connectDB(mongo_uri);
+
+const app : Express = express();
+
+const PORT : string | number = process.env.PORT || 4000;
+
+// allows processing req as json objects
+app.use(express.json());
+// takes all routes after "/api"
+app.use("/api", todoRoutes);
+
+app.listen(PORT, () =>
+    console.log(`Server running on http://localhost:${PORT}`)
+);
